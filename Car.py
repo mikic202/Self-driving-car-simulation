55c8c832cf74e65b3b7925cfb148eb9fb491472c
mchomans <01168876@pw.edu.pl>
2023-06-30 22:01:12 +0200

    add rotation

--- Car.py
+++ Car.py
@@ -2,25 +2,32 @@ import typing
 
 
 class Car:
-    def __init__(self, position: typing.List[int]) -> None:
+    def __init__(self, position: typing.List[float]) -> None:
         self._position = position
         self._speed = []
         self._acceleration = []
+        self._rotation = []
 
-    def set_position(self, position: typing.List[int]) -> None:
+    def set_position(self, position: typing.List[float]) -> None:
         self._position = position
 
-    def position(self) -> typing.List[int]:
+    def position(self) -> typing.List[float]:
         return self._position
 
-    def set_speed(self, speed: typing.List[int]) -> None:
+    def set_speed(self, speed: typing.List[float]) -> None:
         self._speed = speed
 
-    def speed(self) -> typing.List[int]:
+    def speed(self) -> typing.List[float]:
         return self._speed
 
-    def set_acceleration(self, acceleration: typing.List[int]) -> None:
+    def set_acceleration(self, acceleration: typing.List[float]) -> None:
         self._acceleration = acceleration
 
-    def acceleration(self) -> typing.List[int]:
+    def acceleration(self) -> typing.List[float]:
         return self._acceleration
+
+    def set_rotation(self, rotation: typing.List[float]) -> None:
+        self._rotation = rotation
+
+    def rotation(self) -> typing.List[float]:
+        return self._rotation
