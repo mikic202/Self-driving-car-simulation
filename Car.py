19a33e4ab643d9271cd1cd3670f5f5ead1e58f03
mchomans <01168876@pw.edu.pl>
2023-06-30 21:49:02 +0200

    add getters and setters to car class

--- Car.py
+++ Car.py
@@ -12,3 +12,15 @@ class Car:
 
     def position(self) -> typing.List[int]:
         return self._position
+
+    def set_speed(self, speed: typing.List[int]) -> None:
+        self._speed = speed
+
+    def speed(self) -> typing.List[int]:
+        return self._speed
+
+    def set_acceleration(self, acceleration: typing.List[int]) -> None:
+        self._acceleration = acceleration
+
+    def acceleration(self) -> typing.List[int]:
+        return self._acceleration
