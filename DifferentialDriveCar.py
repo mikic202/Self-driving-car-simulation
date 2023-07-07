7fd6f9c297efd9b8e9e08f8f212d87d434f90da9
mchomans <01168876@pw.edu.pl>
2023-07-07 19:22:00 +0200

    refactor the code

--- DifferentialDriveCar.py
+++ DifferentialDriveCar.py
@@ -4,11 +4,11 @@ from math import inf, pi, cos, sin
 
 
 class DifferentialDriveCar(Car):
-    def __init__(self, position: typing.List[float], whele_diametre: float, wheel_distance: float) -> None:
+    def __init__(self, position: typing.List[float], wheel_diametre: float, wheel_distance: float) -> None:
         super().__init__(position)
         self._wheel_rotation_speed = {"right": 0.0, "left": 0.0}  # w układzie odniesienia robota
         self._wheel_speed = {"right": 0.0, "left": 0.0}  # w układzie odniesienia robota
-        self._whele_diametre = whele_diametre
+        self._wheel_diametre = wheel_diametre
         self._instantaneous_center_of_rotation = 0  # w układzie odniesienia robota
         self._wheel_distance = wheel_distance
 
@@ -17,24 +17,10 @@ class DifferentialDriveCar(Car):
 
     def set_wheel_rotation_speed(self, wheel_rotation_speed: typing.Dict[str, float]) -> None:
         self._wheel_rotation_speed = wheel_rotation_speed
-        # self._wheel_speed = {"left": wheel_rotation_speed["left"]*self._whele_diametre, "right": wheel_rotation_speed["right"]*self._whele_diametre}
-        # self._speed = [0, sum(self._wheel_speed.values())/2]
-        # self._angular_velocity = (self._wheel_speed["left"] - self._wheel_speed["right"])/self._wheel_distance
-        # if self._angular_velocity > 0.0001 or self._angular_velocity < -0.0001:
-        #     self._instantaneous_center_of_rotation = self._speed[1]/self._angular_velocity
-        # else:
-        #     self._instantaneous_center_of_rotation = inf
 
     def move_robot_for(self, time_in_s: float) -> None:
-        dl = self._whele_diametre * self._wheel_rotation_speed["left"] * time_in_s
-        dp = self._whele_diametre * self._wheel_rotation_speed["right"] * time_in_s
-        # dfl = self._wheel_rotation_speed["left"] * time_in_s
-        # dfp = self._wheel_rotation_speed["right"] * time_in_s
-        # self._rotation[2] = (self._rotation[2] - (dfl - dfp)*self._wheel_distance)
-        # self._position[0] = self._position[0] + self._whele_diametre * (dfl + dfp)/2*cos(pi*(self._rotation[2]))
-        # self._position[1] = self._position[1] + self._whele_diametre * (dfl + dfp)/2*sin(pi*(self._rotation[2]))
-        # delta_fi = (dl - dp)/(2 * self._wheel_distance)
-        # delta_p = (dl + dp)/2
+        dl = self._wheel_diametre * self._wheel_rotation_speed["left"] * time_in_s
+        dp = self._wheel_diametre * self._wheel_rotation_speed["right"] * time_in_s
 
         fix = (dp + dl)/2
         fifi = (dp - dl)/self._wheel_distance
@@ -43,18 +29,13 @@ class DifferentialDriveCar(Car):
         else:
             d = 2 * fix/fifi*sin(fifi/2)
             R = fix/fifi
-            # print(R)
         self._position[0] += fix*cos(self._rotation[2] + fifi/2)
         self._position[1] += fix*sin(self._rotation[2] + fifi/2)
         self._rotation[2] += fifi
         self._rotation[2] = self._rotation[2] % (2*pi)
 
+    def wheel_diametre(self) -> int:
+        return self._wheel_diametre
 
-if __name__ == "__main__":
-    robot = DifferentialDriveCar([0, 0, 0], 1.0, 2.0)
-    robot.set_wheel_rotation_speed({"right": 1, "left": 0})
-    robot.move_robot_for(28)
-    # robot.move_robot_for(4)
-    # robot.move_robot_for(1)
-    # robot.move_robot_for(1)
-    print(robot.position())
+    def wheel_distance(self) -> int:
+        return self._wheel_distance
--- Visualization.py
+++ Visualization.py
@@ -2,15 +2,20 @@ import sys
 import pygame
 from math import pi
 from DifferentialDriveCar import DifferentialDriveCar
-import threading
+from time import time
+
+BACKGROUND_COLOR = (255, 255, 255)
+DIAGNOSTIC_CIRCLE_COLOR = (240, 240, 240)
+METER_TO_PIXEL_RATIO = 15
 
 
 class Visualization:
     def __init__(self) -> None:
         self._WIN = pygame.display.set_mode((600, 600))
-        self._car = DifferentialDriveCar([0, 0, 0], 2.0, 6.0)
+        self._car = DifferentialDriveCar([10, 10, 0], 2.0, 6.0)
         self.image = pygame.image.load('abc.png')
         self._draw_diagnostocs = False
+        self._last_car_update = time()
         pygame.init()
         self._start()
 
@@ -19,7 +24,7 @@ class Visualization:
             self._check_quit()
             self._check_keystrokes()
             self._move_robot()
-            self._WIN.fill((0, 0, 0))
+            self._WIN.fill(BACKGROUND_COLOR)
             self._draw_diagnostics()
             self._draw_car()
             pygame.display.flip()
@@ -40,7 +45,9 @@ class Visualization:
             self._draw_diagnostocs = False
 
     def _move_robot(self):
-        self._car.move_robot_for(0.005)
+        current_time = time()
+        self._car.move_robot_for(current_time - self._last_car_update)
+        self._last_car_update = current_time
 
     def _check_quit(self):
         for event in pygame.event.get():
@@ -50,15 +57,15 @@ class Visualization:
 
     def _draw_diagnostics(self):
         if self._draw_diagnostocs:
-            pygame.draw.circle(self._WIN, (123, 123, 123), (self._car.position()[0]*15 + 100, self._car.position()[1]*15 + 100), self._car._wheel_distance/2*15)
+            pygame.draw.circle(self._WIN, DIAGNOSTIC_CIRCLE_COLOR, (self._car.position()[0]*METER_TO_PIXEL_RATIO, self._car.position()[1]*METER_TO_PIXEL_RATIO), self._car.wheel_distance()/2*METER_TO_PIXEL_RATIO)
 
     def _draw_car(self):
         w, h = self.image.get_size()
-        self.blitRotate(self.image, (self._car.position()[0]*15 + 100, self._car.position()[1]*15 + 100), (w/2, h/2), -(self._car._rotation[2]/(2 * pi)*360)%360)
+        self.rotate_car(self.image, (self._car.position()[0]*METER_TO_PIXEL_RATIO, self._car.position()[1]*METER_TO_PIXEL_RATIO), (w/2, h/2), -(self._car.rotation()[2]/(2 * pi)*360) % 360)
 
-    def blitRotate(self, image, pos, originPos, angle):
+    def rotate_car(self, image, pos, originPos, angle):
 
-        image_rect = image.get_rect(topleft = (pos[0] - originPos[0], pos[1]-originPos[1]))
+        image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1]-originPos[1]))
         offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
 
         rotated_offset = offset_center_to_pivot.rotate(-angle)
@@ -66,10 +73,11 @@ class Visualization:
         rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
 
         rotated_image = pygame.transform.rotate(image, angle)
-        rotated_image_rect = rotated_image.get_rect(center = rotated_image_center)
+        rotated_image_rect = rotated_image.get_rect(center=rotated_image_center)
 
         self._WIN.blit(rotated_image, rotated_image_rect)
 
 
 if __name__ == "__main__":
+    # threading.Thread(target=Visualization())
     win = Visualization()
