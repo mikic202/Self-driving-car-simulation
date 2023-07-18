e8cb5c74c3bf312387ec9c64ebb5a1adf6158272
mchomans <01168876@pw.edu.pl>
2023-07-18 18:44:31 +0200

    add setting start position and gates

--- TrackCreator.py
+++ TrackCreator.py
@@ -14,16 +14,20 @@ run = True
 line_start = [0, 0]
 
 parser = argparse.ArgumentParser("TrackCreator")
-parser.add_argument("-f", "--file", type=str, default="track.json", help="defines output file name (without extension)")
+parser.add_argument("-f", "--file", type=str, default="track", help="defines output file name (without extension)")
 args = parser.parse_args()
 
 
 class LineCreator:
     def __init__(self, output_file: str) -> None:
         self._line_being_drawn = False
+        self._gate_being_drawn = False
+        self._gates_drawn = []
+        self._curent_gate_start = []
         self._lines_drawn = []
         self._curent_lines_start = []
         self._output_file = output_file
+        self._start_point = None
 
     def _start_line(self, new_line_start: list()):
         self._curent_lines_start = new_line_start
@@ -34,21 +38,45 @@ class LineCreator:
         self._line_being_drawn = False
         self._curent_lines_start = ()
 
+    def add_start_point(self, start_point: list()):
+        self._start_point = start_point
+
     def add_line_point(self, new_line_pos: list()):
+        if self._gate_being_drawn:
+            return
         if self._line_being_drawn:
             self._end_line(new_line_pos)
         else:
             self._start_line(new_line_pos)
 
-    def draw_lines(self, window, mouse_pos):
+    def add_gate(self, line_point: list()):
+        if self._line_being_drawn:
+            return
+        if self._gate_being_drawn:
+            self._gates_drawn.append({LINE_START: self._curent_gate_start, LINE_END: line_point})
+            self._gate_being_drawn = False
+            self._curent_gate_start = ()
+        else:
+            self._curent_gate_start = line_point
+            self._gate_being_drawn = True
+
+    def draw_track(self, window, mouse_pos):
         for line in self._lines_drawn:
             pygame.draw.line(window, "white", line[LINE_START], line[LINE_END])
         if self._line_being_drawn:
             pygame.draw.line(window, "white", self._curent_lines_start, mouse_pos)
 
+        for gate in self._gates_drawn:
+            pygame.draw.line(window, "green", gate[LINE_START], gate[LINE_END])
+        if self._gate_being_drawn:
+            pygame.draw.line(window, "green", self._curent_gate_start, mouse_pos)
+
+        if self._start_point:
+            pygame.draw.circle(window, (255, 100, 100), self._start_point, 8)
+
     def write_track_to_json(self):
         with open(self._output_file + ".json", "w") as outfile:
-            json.dump(self._lines_drawn, outfile)
+            json.dump({"track": self._lines_drawn, "start point": self._start_point, "gates": self._gates_drawn}, outfile)
 
 
 test = LineCreator(args.file)
@@ -57,15 +85,21 @@ while run:
 
     cpt = pygame.mouse.get_pos()
     clock.tick(250)
+    keys_press = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             test.write_track_to_json()
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            test.add_line_point(cpt)
+        if event.type == pygame.MOUSEBUTTONDOWN and keys_press[pygame.K_LSHIFT]:
+            test.add_start_point(cpt)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                test.add_line_point(cpt)
+            if event.button == 3:
+                test.add_gate(cpt)
 
     window.fill("black")
-    test.draw_lines(window, cpt)
+    test.draw_track(window, cpt)
     pygame.draw.circle(window, "white", cpt, r, 3)
     pygame.display.flip()
 
