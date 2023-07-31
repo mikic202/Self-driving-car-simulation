32c27540bd75411593850c9a7ce4f2a25d3e7926
mchomans <01168876@pw.edu.pl>
2023-07-31 20:27:22 +0200

    add init tests

--- tests/test_line.py
+++ tests/test_line.py
@@ -6,3 +6,18 @@ def test_init_line_regular():
     assert(line._start == [5, 5])
     assert(line._end == [10, 10])
 
+
+def test_init_line_reversed():
+    line = Line([10, 10], [5, 5])
+    assert(line._start == [5, 5])
+    assert(line._end == [10, 10])
+
+
+def test_init_line_check_a():
+    line = Line([5, 7], [10, 18])
+    assert(line._a == (7 - 18)/(5 - 10))
+
+
+def test_init_line_check_b():
+    line = Line([5, 7], [10, 18])
+    assert(line._b == (7 - (7 - 18)/(5 - 10)*5))
